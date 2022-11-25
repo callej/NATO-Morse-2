644a0dd9aeff97c13e45ee1f6a6a9f9b27d608f6
Calle Johansson <callej@live.com>
2022-11-24 19:20:57 -0800

    Initial commit. No functioning code in this commit.

---
+++ main.py
@@ -0,0 +1,12 @@
+from tokens import TOKENS
+
+
+def main():
+    print(TOKENS["a"]["nato"])
+    print(TOKENS["a"]["morse"])
+    print(TOKENS["b"]["nato"])
+    print(TOKENS["b"]["morse"])
+
+
+if __name__ == "__main__":
+    main()
---
+++ tokens.py
@@ -0,0 +1,20 @@
+
+
+TOKENS = {
+    "a": {
+        "nato": "ALFA",
+        "morse": "-.-"
+    },
+    "b": {
+        "nato": "BRAVO",
+        "morse": "---"
+    },
+    "c": {
+        "nato": "ALFA",
+        "morse": "-.-"
+    },
+    "d": {
+        "nato": "BRAVO",
+        "morse": "---"
+    }
+}
