7a9105a03e17d8ad30debda69cd5e0f3cd76ecf1
Calle Johansson <callej@live.com>
2022-11-26 10:56:32 -0800

    Added Female Voice
    It is now possible to choose between a male or a female voice for reading the NATO Phonetics of the text.
    
    The male voice is using Windows speech engine. The female voice is using Google speech engine. Which voice to use is configured in the Speech Properties section at the top of the file.
    
    Sections at the top of the file list configuration properties for printed text, Morse, and speech in different sections to make it easy to modify most behavior of the program without having to change any code.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from winsound import Beep
 from time import sleep
 from win32com.client import Dispatch
 from symbols import SYMBOLS
+from mutagen.mp3 import MP3
+from gtts import gTTS
+from pygame import mixer
 
 # ***   Text Properties   *** #
 BOLD = "\x1b[1m"
@@ -9,38 +12,64 @@ TXT = "\x1b[38;2;0;0;210;48;2;212;175;55m"
 NORMAL = "\x1b[m"
 
 # ***   Morse Properties   *** #
+PLAY_MORSE = True
 DOT = 1
 DASH = 3
 SYMBOL_SPACE = 1
 LETTER_SPACE = 3
 WORD_SPACE = 7
-WPM = 20                        # Words per minute (PARIS)
-DOT_DURATION = 1200 / WPM      # Duration in milli seconds
+WPM = 20                    # Words per minute (PARIS)
+DOT_DURATION = 1200 / WPM   # Duration in milli seconds
 FREQ = 400
+
+# ***   Speech Properties   *** #
+SPEAK = True
+MALE = False
+LANG = "en"
+DIALECT = "com"
+LANG_CHECK = False
+SPEED = False
+PAUSE = ","
+SPEECH_FILE = "speach_tmp.mp3"
+
+
+def windows_voice(text):
+    speak = Dispatch("SAPI.SpVoice")
+    for word in text.split(PAUSE):
+        speak.Speak(word)
+
+
+def google_voice(text):
+    gTTS(text, lang=LANG, tld=DIALECT, lang_check=LANG_CHECK, slow=SPEED).save(SPEECH_FILE)
+    mixer.init()
+    mixer.music.load(SPEECH_FILE)
+    mixer.music.play()
+    sleep(MP3(SPEECH_FILE).info.length)
 
 
 def print_nato(text):
     print()
     nato_show = ""
-    nato_word = ""
-    nato_speak = []
+    nato_speak = ""
     spaced = False
     for c in text.strip():
         if c.lower() in SYMBOLS:
             nato_show += BOLD + TXT + " " + SYMBOLS[c.lower()]["nato"] + " " + NORMAL
-            nato_word += " " + SYMBOLS[c.lower()]["nato"] + " "
+            nato_speak += " " + SYMBOLS[c.lower()]["nato"] + " "
             spaced = False
         if c.isspace() and not spaced:
             nato_show += "   "
-            nato_speak.append(nato_word.strip())
-            nato_word = ""
+            nato_speak += PAUSE
             spaced = True
-    if not spaced and nato_word != "":
-        nato_speak.append(nato_word.strip())
     print(nato_show.strip())
-    speak = Dispatch("SAPI.SpVoice")
-    for word in nato_speak:
-        speak.Speak(word)
+    if SPEAK:
+        if MALE:
+            windows_voice(nato_speak)
+        elif nato_speak:
+            try:
+                google_voice(nato_speak)
+            except (ValueError, RuntimeError):
+                windows_voice(nato_speak)
 
 
 def send_code(symbol):
@@ -53,18 +82,19 @@ def send_code(symbol):
 
 
 def send_morse(text):
-    print()
-    spaced = False
-    for c in text.strip():
-        if c.lower() in SYMBOLS:
-            print(f'{c}: {SYMBOLS[c.lower()]["morse"]}')
-            send_code(c)
-            sleep((LETTER_SPACE - SYMBOL_SPACE) * DOT_DURATION / 1000.0)
-            spaced = False
-        if c.isspace() and not spaced:
-            print()
-            sleep((WORD_SPACE - LETTER_SPACE) * DOT_DURATION / 1000.0)
-            spaced = True
+    if PLAY_MORSE:
+        print()
+        spaced = False
+        for c in text.strip():
+            if c.lower() in SYMBOLS:
+                print(f'{c}: {SYMBOLS[c.lower()]["morse"]}')
+                send_code(c)
+                sleep((LETTER_SPACE - SYMBOL_SPACE) * DOT_DURATION / 1000.0)
+                spaced = False
+            if c.isspace() and not spaced:
+                print()
+                sleep((WORD_SPACE - LETTER_SPACE) * DOT_DURATION / 1000.0)
+                spaced = True
 
 
 def main():
